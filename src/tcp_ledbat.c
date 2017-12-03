09b5d44b182df59470f0ad0d8ede5266e412cad3
snowmohan <252203262@qq.com>
2017-12-03 17:38:35 -0500

    Update tcp_ledbat.c

--- src/tcp_ledbat.c
+++ src/tcp_ledbat.c
@@ -27,11 +27,11 @@
 /* NOTE: len are the actual length - 1 */
 static int base_histo_len = 10;
 static int noise_filter_len = 4;
-static int target = 1000000;
+static int target = 100;
 static int gain_num = 1;
 static int gain_den = 1;
-static int do_ss = 1;
-static int ledbat_ssthresh = 2;
+static int do_ss = 2;
+static int ledbat_ssthresh = 0x0010;
 
 module_param(base_histo_len, int, 0644);
 MODULE_PARM_DESC(base_histo_len, "length of the base history vector");
