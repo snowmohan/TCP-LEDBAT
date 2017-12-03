4c05fa42740d45ed71e5577b918045466622f157
snowmohan <252203262@qq.com>
2017-12-03 17:31:26 -0500

    Update tcp_ledbat.c

--- src/tcp_ledbat.c
+++ src/tcp_ledbat.c
@@ -28,9 +28,9 @@
 static int base_histo_len = 10;
 static int noise_filter_len = 4;
 static int target = 100;
-static int gain_num = 1;
+static int gain_num = 2;
 static int gain_den = 1;
-static int do_ss = 0;
+static int do_ss = 1;
 static int ledbat_ssthresh = 2;
 
 module_param(base_histo_len, int, 0644);
