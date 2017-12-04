7c0a50ba24cdc2d9a9f8c263e5bc9c6bb8fffc80
snowmohan <252203262@qq.com>
2017-12-03 22:03:50 -0500

    Update tcp_ledbat.c

--- src/tcp_ledbat.c
+++ src/tcp_ledbat.c
@@ -254,12 +254,12 @@ static void tcp_ledbat_cong_avoid(struct sock *sk, u32 ack, u32 acked)
 	offset = ((s64) target) - (queue_delay);
 	/*printk(KERN_DEBUG
 	       "queue_delay %lld, offset %lld ",
-	       queue_delay,offset);
+	       queue_delay,offset);*/
 	printk(KERN_DEBUG
 	       "time %u, queue_delay %lld, offset %lld cwnd_cnt %u, "
 	       "cwnd %u, delay %lld, min %lld\n",
 	       tcp_time_stamp, queue_delay, offset, ledbat->snd_cwnd_cnt,
-	       tp->snd_cwnd, current_delay, base_delay);*/
+	       tp->snd_cwnd, current_delay, base_delay);
 	offset *= gain_num;
 	offset = do_div(offset, gain_den);
 
