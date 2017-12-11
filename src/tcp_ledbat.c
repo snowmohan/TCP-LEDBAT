6ae615ad1cfa3488c983f184be20294f419ca2e1
snowmohan <252203262@qq.com>
2017-12-11 15:42:54 -0500

    Update tcp_ledbat.c

--- src/tcp_ledbat.c
+++ src/tcp_ledbat.c
@@ -19,7 +19,7 @@
 #define LP_RESOL       1000
 
 #define  DEBUG_SLOW_START    0
-#define  DEBUG_DELAY         0
+#define  DEBUG_DELAY         1
 #define  DEBUG_OWD_HZ        0
 #define  DEBUG_NOISE_FILTER  0
 #define  DEBUG_BASE_HISTO    0
